9bfca2b2e67496915ab5e8a2cc43402d1414563c
tanvir841 <ab>
2019-12-18 01:16:25 -0800

    ep 2

--- programmaticalDesign/ViewController.swift
+++ programmaticalDesign/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let pawImageView: UIImageView = {
      let imageView = UIImageView(image: #imageLiteral(resourceName: "paw"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     let descriptionTextView: UITextView = {
@@ -31,11 +32,30 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(pawImageView)
         view.addSubview(descriptionTextView)
+        
         setupLayout()
         //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         // Do any additional setup after loading the view.
     }
     private func setupLayout() {
+        
+        let topImageContainerView = UIView()
+            topImageContainerView.backgroundColor = .blue
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(pawImageView)
+        pawImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        topImageContainerView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        //        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
         pawImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         pawImageView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
         pawImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
